fdab4eff18990293d19030be1f259dc7ca9f1f16
Amadeo Malko Rivadeneira <165217370+AmadeuxXX7@users.noreply.github.com>
2026-07-10 12:37:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from tkinter import Tk, Frame, Button
-from bloques import createBlocks, Play
+import tkinter as tk
+from bloques import createBlocks, code
+from codes import Play
 
 window = Tk()
+window.title("BlockX Beta")
 window.geometry("800x500")
-window.title("BlockX")
+icon = tk.PhotoImage(file="imgs/logo2.png")
+window.iconphoto(True, icon)
 
 paleta = Frame(window, bg="grey", width=200)
 paleta.place(x=0, y=0, relheight=1.0)
 paleta.config(borderwidth=10, relief="raised")
 paleta.grid_propagate(False)
-workspace = Frame(window, bg="white")
-workspace.place(x=200, y=0, relwidth=1.0, relheight=1.0, anchor="nw")
+consolaDeBloques = Frame(window, bg="white")
+consolaDeBloques.place(x=200, y=0, relwidth=1.0, relheight=1.0, anchor="nw")
 
 block_counter = {"i": 0}
-createBlocks(paleta, workspace, block_counter)
+createBlocks(paleta, consolaDeBloques, block_counter)
 
-play = Button(window, text="▶️", command=Play, bg="grey")
-play.place(relx=1.0, anchor="ne")
+play = Button(window, text="▶️", command=lambda:Play(code), bg="grey")
+play.pack(anchor="ne")
+
+"""def seeCode(): print(code)
+seecode = Button(window, text="📜", command=seeCode, bg="grey")
+seecode.pack(pady=5, anchor="ne")"""
+
 
 window.mainloop()
