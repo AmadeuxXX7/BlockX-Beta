97fa449cd7b781e7aadf041278977db3694827f6
Amadeo Malko Rivadeneira <165217370+AmadeuxXX7@users.noreply.github.com>
2026-07-22 14:57:14 -0500

    Delete codes.py

--- codes.py
+++
@@ -1,9 +0,0 @@
-import time, os
-
-def Play(code):
-    print("Play")
-    for block_type, entry in code:
-        if block_type == "print":
-            print(entry.get())
-        elif block_type == "wait":
-            time.sleep(float(entry.get()))
